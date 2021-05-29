e2891731717ac3be2a93ce35666303c2ccff0d32
Ammar Bohra <bohraammar@gmail.com>
2021-05-29 16:56:42 +0530

    animated lotus

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,9 @@
-gsap.defaults({duration: 4, ease: "power4.out"})
+gsap.defaults({duration: 2, ease: "power4.out"})
 let tl = gsap.timeline({repeat: -1, repeatDelay: 0});
-tl.to('#lotus', {scale: 0.9, duration: 2})
-tl.fromTo('#lotus-waves',{scale: 1, opacity: 1}, {scale: 1.5, opacity: 0}, 0)
-tl.to('#lotus', {scale: 1, duration: 2, ease: "power4.inOut"}, 2)
+tl.to('#lotus', {scale: 0.9})
+tl.to('#lotus-waves', {scale: 1.5, duration: 4}, 0)
+tl.to('#lotus-waves', {opacity: 0, ease: 'none'}, 1)
+tl.to('#lotus', {scale: 1, ease: "power4.inOut"}, 2)
 // tl.set('#lotus-waves',{scale: 1, opacity: 1})
 // tl.to('#lotus-waves',{scale: 1, opacity: 1, duration: 2, ease: "power4.in"}, "<")
 // tl.to('#lotus-waves', {opacity: 0, duration: 2}, "<2")
